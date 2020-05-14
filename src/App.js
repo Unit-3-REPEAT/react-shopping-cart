b812534f4458d3314bfe65e6935dab883b5194e3
Dino-Muratovic <dinomuratovic1989@gmail.com>
2020-05-14 14:27:54 -0500

    Created Product Context and linked everything together

--- src/App.js
+++ src/App.js
@@ -6,27 +6,39 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import {ProductContext} from './contexts/ProductContext';
+
 
 function App() {
 	const [products] = useState(data);
+	// console.log(`products-->`, products)
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart({...cart,item})
 	};
 
 	return (
-		<div className="App">
+		<div className="App">			
 			<Navigation cart={cart} />
+
+
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				{/* <Products products={products} addItem={addItem} /> --THIS WAS PREVIOUS STATE BEFORE CONTEXT*/} 
+				<ProductContext.Provider value={{products, addItem}}>
+					<Products />{/*CURRENT STATE WITH CONTEXT*/ }
+				</ProductContext.Provider>
 			</Route>
+
+
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			
 		</div>
 	);
 }
